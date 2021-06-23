7135c411bcd8920fedc149cbe69d968fe414be4a
Alina Navarko <anavarko@aras.com>
2021-06-23 19:44:08 +0300

    L5: added Task3

--- Practical_Tasks/L5_Task1/Program.cs
+++ Practical_Tasks/L5_Task1/Program.cs
@@ -8,6 +8,7 @@ namespace L5_Task1
         {
             Console.WriteLine("Task1: An array can't be empty. Anyway it will be filled out with default values of the corresponding type"); //TASK 1
             MethodTask2();
+            MethodTask3();
         }
 
         private static void MethodTask2()
@@ -33,6 +34,42 @@ namespace L5_Task1
             {
                 Console.WriteLine(i + ": " + arrayTypes[i]);
             }
+         }
+
+        private static void MethodTask3()
+        {
+            int value;
+            int number;
+            int arraySize = 13;
+
+            Random arrayRnd = new Random();
+            int[] arrayNum = new int[arraySize];
+            Console.WriteLine("Task3: Random array:");
+
+            for (int i = 0; i < arrayNum.Length; i++)
+            {
+                arrayNum[i] = arrayRnd.Next(100);
+                Console.Write(arrayNum[i] + " " + "| ");
+            }
+            
+            number = 0;
+            value = arrayNum[number];
+
+            for (int i = 1; i < arrayNum.Length; i++)
+            {
+                if (arrayNum[i] > value)
+                {
+                    value = arrayNum[i];
+                    number = i;
+                }
+            }
+            Console.WriteLine("The largest Value is: " + value);
         }
-    }
+
+        /*
+        private static void MethodTask4()
+        {
+            short shortValue = 100;
+            object obj = shortValue */
+        }
 }
